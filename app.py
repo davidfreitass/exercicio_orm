aaf1a64cae038017b353e096def6bc9433053a2c
davidfreitass <davidcnt@outlook.com>
2023-08-17 15:46:18 -0300

    Importando bibliotecas

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-#TESTE.
+from sqlalchemy import ForeignKey, create_engine
+from sqlalchemy.engine import URL
+from sqlalchemy import Column, Integer, String, DateTime, Text
+from sqlalchemy.orm import declarative_base
+from datetime import datetime
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import sessionmaker
+
+url = "sqlite:///database.db"
+engine = create_engine(url)
+connection = engine.connect()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
